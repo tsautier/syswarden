ed5c070515d13919921a884743d63a5f000238a2
🔐Laurent M🔐 <laurent@data-shield.eu>
2026-05-22 08:28:29 +0200

    ⚡ 0.36.12 ⚡ : Minor fixes (Please, check release notes for more info)

--- src/universel/generate_dashboard.sh
+++ src/universel/generate_dashboard.sh
@@ -45,7 +45,7 @@ generate_dashboard() {
 set -euo pipefail
 
 # --- VERSION CONFIGURATION ---
-SYSWARDEN_VERSION="v0.36.11"
+SYSWARDEN_VERSION="v0.36.12"
 
 DATA_FILE="/etc/syswarden/ui/data.json"
 
